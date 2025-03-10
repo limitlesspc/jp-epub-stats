f182f10a35f064f06f1ff4334d5485160863bc1e
limitlesspc <48165199+limitlesspc@users.noreply.github.com>
2025-03-10 14:54:29 -0400

    feat: add `--csv` option for csv file output

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
+import { parseArgs } from "@std/cli/parse-args";
 import * as path from "@std/path";
-import loadEpub from "./load-epub.ts";
 import stringWidth from "string-width";
+import loadEpub from "./load-epub.ts";
 
-const filePaths = Deno.args;
+const args = parseArgs(Deno.args, {
+  boolean: ["csv"],
+});
+
+const filePaths = args._.map(String);
 const files = filePaths.map((filePath) => {
   const { dir, name } = path.parse(filePath);
   return {
@@ -10,46 +15,68 @@ const files = filePaths.map((filePath) => {
     title: dir ? `${dir}/${name}` : name,
   };
 });
-const longestFileNameWidth = Math.max(
-  ...files.map(({ title }) => stringWidth(title)),
-);
 
-const spacing = 4;
-const headers = [
-  { label: "title", width: longestFileNameWidth },
-  { label: "characters" },
-  { label: "unique kanji" },
-  { label: "kanji used once" },
-  { label: "kanji used once (%)" },
-];
+if (args.csv) {
+  console.log(
+    "title,characters,unique kanji,kanji used once,kanji used once (%)",
+  );
 
-let header = "";
-for (const [i, { label, width = label.length }] of headers.entries()) {
-  header += label;
-  if (i < headers.length - 1) {
-    header += " ".repeat(width - label.length + spacing);
+  for (const { path, title } of files) {
+    const { characters, uniqueKanji, uniqueKanjiUsedOnce } =
+      await loadEpub(path);
+
+    const rowData = [
+      title,
+      characters,
+      uniqueKanji,
+      uniqueKanjiUsedOnce,
+      `${Math.round((uniqueKanjiUsedOnce / uniqueKanji) * 100)}%`,
+    ];
+    console.log(rowData.join(","));
   }
-}
-console.log(header);
-console.log("-".repeat(header.length));
-for (const { path, title } of files) {
-  const { characters, uniqueKanji, uniqueKanjiUsedOnce } = await loadEpub(path);
+} else {
+  const longestFileNameWidth = Math.max(
+    ...files.map(({ title }) => stringWidth(title)),
+  );
 
-  const rowData = [
-    title,
-    characters,
-    uniqueKanji,
-    uniqueKanjiUsedOnce,
-    `${Math.round((uniqueKanjiUsedOnce / uniqueKanji) * 100)}%`,
+  const spacing = 4;
+  const headers = [
+    { label: "title", width: longestFileNameWidth },
+    { label: "characters" },
+    { label: "unique kanji" },
+    { label: "kanji used once" },
+    { label: "kanji used once (%)" },
   ];
-  let row = "";
+
+  let header = "";
   for (const [i, { label, width = label.length }] of headers.entries()) {
-    const data = rowData[i] || "";
-    const str = data.toString();
-    row += str;
+    header += label;
     if (i < headers.length - 1) {
-      row += " ".repeat(width - stringWidth(str) + spacing);
+      header += " ".repeat(width - label.length + spacing);
     }
   }
-  console.log(row);
+  console.log(header);
+  console.log("-".repeat(header.length));
+  for (const { path, title } of files) {
+    const { characters, uniqueKanji, uniqueKanjiUsedOnce } =
+      await loadEpub(path);
+
+    const rowData = [
+      title,
+      characters,
+      uniqueKanji,
+      uniqueKanjiUsedOnce,
+      `${Math.round((uniqueKanjiUsedOnce / uniqueKanji) * 100)}%`,
+    ];
+    let row = "";
+    for (const [i, { label, width = label.length }] of headers.entries()) {
+      const data = rowData[i] || "";
+      const str = data.toString();
+      row += str;
+      if (i < headers.length - 1) {
+        row += " ".repeat(width - stringWidth(str) + spacing);
+      }
+    }
+    console.log(row);
+  }
 }
